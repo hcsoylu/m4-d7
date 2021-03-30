b7c6b5858218e73720cfc7eefb84ad8a6aa53e9b
Can Soylu <hsyncn17@gmail.com>
2021-03-30 17:42:43 +0300

    added new js

--- src/components/DishComments.jsx
+++ src/components/DishComments.jsx
@@ -1,18 +1,21 @@
-import { ListGroup } from 'react-bootstrap'
+import { ListGroup } from "react-bootstrap";
+import React from "react";
 
-const DishComments = (props) =>
-    props.selectedDish
-        ? <ListGroup>
-            <h2>Comments for {props.selectedDish.name}</h2>
-            {
-                props.selectedDish.comments.map(c => (
-                    <ListGroup.Item key={c.id}>{c.comment}</ListGroup.Item>
-                ))
-            }
-        </ListGroup>
-        : <h1>NOTHING YET</h1>
+class DishComments extends React.Component {
+  render() {
+    return this.props.selectedDish ? (
+      <ListGroup>
+        <h2>Comments for {this.props.selectedDish.name}</h2>
+        {this.props.selectedDish.comments.map((c) => (
+          <ListGroup.Item key={c.id}>{c.comment}</ListGroup.Item>
+        ))}
+      </ListGroup>
+    ) : (
+      <h1>NOTHING YET</h1>
+    );
+  }
+}
 
-
-export default DishComments
+export default DishComments;
 
 // mapping props.selectedDish.comments
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,19 +1,32 @@
-import { Navbar, Nav } from 'react-bootstrap'
+import { Navbar, Nav } from "react-bootstrap";
+import React from "react";
 
-const NavBar = (props) => (
-    <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
-        <Navbar.Brand href="#home">{props.title} - Strive for food</Navbar.Brand>
+class NavBar extends React.Component {
+  constructor(props) {
+    super(props);
+    console.log("Heyy I'm the NavBar constructor");
+  }
+  render() {
+    return (
+      <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+        <Navbar.Brand href="#home">
+          {this.props.title} - Strive for food
+        </Navbar.Brand>
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
         <Navbar.Collapse id="responsive-navbar-nav">
-            <Nav className="ml-auto">
-                {
-                    props.links.map(link => <Nav.Link key={link} href={'#' + link.toLowerCase()}>{link}</Nav.Link>)
-                }
-            </Nav>
+          <Nav className="ml-auto">
+            {this.props.links.map((link) => (
+              <Nav.Link key={link} href={"#" + link.toLowerCase()}>
+                {link}
+              </Nav.Link>
+            ))}
+          </Nav>
         </Navbar.Collapse>
-    </Navbar>
-)
+      </Navbar>
+    );
+  }
+}
 
-export default NavBar
+export default NavBar;
 
 // functional components are a touch faster than class based ones
